e8aa7c7d271416ca4077cbac413406cfbef90473
jbonhamwest <jbwest@Janices-MacBook-Pro.local>
2014-06-03 21:31:46 -0400

    Guessing Game

---
+++ Guess_the_Number_Game/Game_1_to_100.rb
@@ -0,0 +1,26 @@
+class Guessing_game
+  def initialize
+    prng = Random.new
+    @number = prng.rand(1..100)
+  end
+
+  def play
+    puts 'Let the games begin!'
+    answer = 0
+    while @number != answer
+      puts 'Type in a numeral between 1 and 100. '
+      answer = gets.chomp.to_i
+      if answer < @number
+        puts 'Your number is less than the computer\'s number.'
+      elsif answer > @number
+        puts 'Your number is greater than the computer\'s number.'
+      elsif answer == @number
+        puts 'YOU win the prize. Your guess is correct!'
+      end
+    end
+  end
+end
+
+game = Guessing_game.new
+
+game.play
